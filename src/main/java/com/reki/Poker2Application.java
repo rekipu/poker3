fef638c45cc613af9ece9cf92a98fad8598e44ff
rekipu <reki667@hotmail.com>
2019-09-19 10:44:12 +0200

    primer commit

---
+++ src/main/java/com/reki/Poker2Application.java
@@ -0,0 +1,13 @@
+package com.reki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poker2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poker2Application.class, args);
+	}
+
+}
