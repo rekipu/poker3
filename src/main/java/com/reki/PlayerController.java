25aeb8b04b14e8a2853f72354091d3cadf2b86b9
rekipu <reki667@hotmail.com>
2019-09-19 12:05:32 +0200

    canvis al controller

--- src/main/java/com/reki/PlayerController.java
+++ src/main/java/com/reki/PlayerController.java
@@ -3,6 +3,7 @@ package com.reki;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,48 +13,43 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PlayerController {
-	
+
 	private final PlayerRepository repository;
-	
-	PlayerController(PlayerRepository repository){
+
+	PlayerController(PlayerRepository repository) {
 		this.repository = repository;
 	}
+
 	@GetMapping("/players")
-	  List<Player> all() {
-	    return (List<Player>) repository.findAll();
-	  }
+	List<Player> all() {
+		return (List<Player>) repository.findAll();
+	}
 
-	  @PostMapping("/players")
-	  Player newPlayer(@RequestBody Player newPlayer) {
-	    return repository.save(newPlayer);
-	  }
+	@PostMapping("/players/{id}")
+	Player newPlayer(@RequestBody String id) {
+		Player player = new Player(id);
+		return repository.save(player);
+	}
 
-	  // Single item
+	// Single item
 
-	  @GetMapping("/players/{id}")
-	  Optional<Player> one(@PathVariable Long id) {
-	    return repository.findById(id);
-	  }
+	@GetMapping("/players/{id}")
+	Optional<Player> one(@PathVariable Long id) {
+		return repository.findById(id);
+	}
 
-	  @PutMapping("/players/{id}")
-	  Player replaceEmployee(@RequestBody Player newPlayer, @PathVariable Long id) {
+	/*
+	 * @PutMapping("/players/{id}") Player replaceEmployee(@RequestBody Player
+	 * newPlayer, @PathVariable Long id) { return repository.findById(id)
+	 * .map(player -> { player.setName(newPlayer.getName());
+	 * player.setDate(newPlayer.getDate()); return repository.save(player); })
+	 * .orElseGet(() -> { newPlayer.setId(id); return repository.save(newPlayer);
+	 * }); }
+	 */
 
-	    return repository.findById(id)
-	      .map(player -> {
-	        player.setName(newPlayer.getName());
-	        player.setDate(newPlayer.getDate());
-	        return repository.save(player);
-	      })
-	      .orElseGet(() -> {
-	    	  newPlayer.setId(id);
-	        return repository.save(newPlayer);
-	      });
-	  }
-
-	  @DeleteMapping("/employees/{id}")
-	  void deleteEmployee(@PathVariable Long id) {
-	    repository.deleteById(id);
-	  }
-	
+	@DeleteMapping("/players/{id}")
+	void deletePlayer(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
 
 }
