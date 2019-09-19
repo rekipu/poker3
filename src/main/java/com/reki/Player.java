9d37ae83fbc6518397ad6b6b05ebba4e9fb085d9
rekipu <reki667@hotmail.com>
2019-09-19 11:42:53 +0200

    afegits player, controller i repository

---
+++ src/main/java/com/reki/Player.java
@@ -0,0 +1,24 @@
+package com.reki;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Player {
+	private @Id @GeneratedValue Long id;
+	private String name;
+	private LocalDate regDate;
+
+	Player(){}
+	
+	Player(String name){
+		this.name = name;
+		this.regDate = LocalDate.now();
+	}
+}
---
+++ src/main/java/com/reki/PlayerRepository.java
@@ -0,0 +1,7 @@
+package com.reki;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PlayerRepository extends CrudRepository<Player, Long>{
+
+}
